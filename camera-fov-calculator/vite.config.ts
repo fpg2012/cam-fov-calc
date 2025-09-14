047e97ef8ca52fcdae296d23998b1b67280cf4b6
nth233 <fpg2012@foxmail.com>
2025-09-14 23:51:58 +0800

    update

--- camera-fov-calculator/vite.config.ts
+++ camera-fov-calculator/vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/cam-fov-calc/',
+  server: {
+    open: '/cam-fov-calc/'
+  },
   plugins: [react()],
 })
